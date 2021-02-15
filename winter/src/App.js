8f7a3f02a2ff89e6f0e2bdd5848bba480ff05f33
WinterInside <>
2021-02-15 10:43:43 +0300

    prepear to work

--- winter/src/App.js
+++ winter/src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import Fcomponent from'./components/Fcomponent';
 import Ccomponent from './components/Ccomponent';
-import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Form />
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://vk.com/maks_winter"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Моя страница ВКонтакте
-        </a>
-      </header>
     </div>
   );
 }
