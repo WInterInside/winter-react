1ebf9efe1febee2879ce0e6e8ecac038a94db09b
WinterInside <>
2021-03-02 20:56:54 +0300

    layout apt card

--- winter/src/components/apartments/Apartments.js
+++ winter/src/components/apartments/Apartments.js
@@ -6,17 +6,19 @@ export default function Apartments() {
     <section className="apartments">
       <div className="container">
         <h2 className="section__heading">ДОСТУПНЫ ДЛЯ БРОНИРОВАНИЯ:</h2>
-        <div className="apartments__wrapper">
+        <div className="apartments__wrapper apartments__wrapper--column">
           <a href="#" className="apartments__link">Больше варинатов:</a>
           <ul className="apartments__list">
             <li className="apartments__item">
-              <h3 className="apartments__heading">1</h3>
-            </li>
-            <li className="apartments__item">
-              <h3 className="apartments__heading">2</h3>
-            </li>
-            <li className="apartments__item">
-              <h3 className="apartments__heading">3</h3>
+              <img src="/img/ap-ph.jpg" alt="photo" className="apartments__photo"/>
+              <div className="apartments__wrapper">
+                <h3 className="apartments__heading">Апартаменты</h3>
+                <p className="apartments__prise">от 1000 р.</p>
+              </div>
+              <p className="apartments__discription">Отдельностоящий дом со всеми удобствами. Подходит для большой семьи.</p>
+              <div className="apartments__wrapper">
+                <a className="apartments__link" href="http://">Подробнее</a>
+              </div>
             </li>
           </ul>
         </div>
