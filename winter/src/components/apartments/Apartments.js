8e6ecae7e18a620392f53010d8bc87eb914ae0ea
WinterInside <>
2021-03-02 22:45:26 +0300

    add apartments card component, layot it

--- winter/src/components/apartments/Apartments.js
+++ winter/src/components/apartments/Apartments.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Apartments.scss";
+import Apartments_card from '../apartments/Apartments_card';;
 
 export default function Apartments() {
   return (
@@ -9,17 +10,9 @@ export default function Apartments() {
         <div className="apartments__wrapper apartments__wrapper--column">
           <a href="#" className="apartments__link">Больше варинатов:</a>
           <ul className="apartments__list">
-            <li className="apartments__item">
-              <img src="/img/ap-ph.jpg" alt="photo" className="apartments__photo"/>
-              <div className="apartments__wrapper">
-                <h3 className="apartments__heading">Апартаменты</h3>
-                <p className="apartments__prise">от 1000 р.</p>
-              </div>
-              <p className="apartments__discription">Отдельностоящий дом со всеми удобствами. Подходит для большой семьи.</p>
-              <div className="apartments__wrapper">
-                <a className="apartments__link" href="http://">Подробнее</a>
-              </div>
-            </li>
+            <Apartments_card />
+            <Apartments_card />
+            <Apartments_card />
           </ul>
         </div>
       </div>
