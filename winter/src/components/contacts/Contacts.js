d9e661a4cd7c4b1ef3670f8f4e97aa436c5d9d3d
WinterInside <>
2021-03-07 13:03:58 +0300

    some fixes in cont

--- winter/src/components/contacts/Contacts.js
+++ winter/src/components/contacts/Contacts.js
@@ -21,7 +21,7 @@ export default function Contacts() {
         <li className="contacts__item">
           <a className="contacts__link contakts__link--tel" href="tel:+1234567890"> 
             <tel className="contacts__number">+7 978 *** ** ** </tel>
-            <svg className="contacts__icon" width="10" height="15" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className="contacts__icon contacts__icon--tel" width="10" height="15" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M0.469889 0C0.213718 0 0 0.233132 0 0.51259V11.4874C0 11.7669 0.213718 12 0.469889 12H6.82757C7.08373 12 7.29745 11.7669 7.29745 11.4874V0.51259C7.29745 0.233132 7.08373 0 6.82757 0H0.469889ZM0.610483 0.691164H6.68709V9.90527H0.610492L0.610483 0.691164ZM3.64872 10.469C3.76323 10.469 3.88298 10.5231 3.96395 10.6114C4.04491 10.6997 4.09437 10.8302 4.09437 10.9551C4.09437 11.08 4.04491 11.2105 3.96395 11.2989C3.88298 11.3872 3.76323 11.4413 3.64872 11.4413C3.53422 11.4413 3.41461 11.3872 3.33364 11.2989C3.25267 11.2105 3.20308 11.08 3.20308 10.9551C3.20308 10.8302 3.25267 10.6997 3.33364 10.6114C3.41461 10.5231 3.53422 10.469 3.64872 10.469Z" fill="white"/>
             </svg>
           </ a>
