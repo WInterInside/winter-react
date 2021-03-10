6ba1f44e940012110bb27ba4360704dcb771d300
WinterInside <>
2021-03-10 21:58:47 +0300

    mobile layot

--- winter/src/components/apartments/Apartments.js
+++ winter/src/components/apartments/Apartments.js
@@ -6,9 +6,9 @@ export default function Apartments() {
   return (
     <section className="apartments">
       <div className="container container--padding0">
-        <h2 className="section__heading">ДОСТУПНЫ ДЛЯ БРОНИРОВАНИЯ:</h2>
+        <h2 className="section__heading section__heading--apartments">ДОСТУПНЫ ДЛЯ БРОНИРОВАНИЯ:</h2>
         <div className="apartments__wrapper apartments__wrapper--column">
-          <a href="#" className="apartments__link">Больше варинатов:</a>
+          <a href="#" className="apartments__link apartments__link--more">Больше варинатов:</a>
           <ul className="apartments__list">
             <ApartmentsCard />
             <ApartmentsCard />
