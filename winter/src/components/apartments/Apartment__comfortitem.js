f3b07b50caf788d3d475100a073fb1aac1ed2604
WinterInside <>
2021-04-02 22:06:10 +0300

    prepear icons to apart page

--- winter/src/components/apartments/Apartment__comfortitem.js
+++ winter/src/components/apartments/Apartment__comfortitem.js
@@ -3,8 +3,9 @@ import "./Apartments.scss";
 
 export default function Apartments__comfortitem(props) {
   return (  
-    <li className="apartment__comfortitem">
-      {props.comfort}
+    <li className="apartment__comfortitem">      
+      <div className="apartment__icon">1</div>
+      <p className="apartment__comfortdesc">{props.comfort}</p>
     </li>
   )
 }
